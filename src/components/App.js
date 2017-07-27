ec53432a7417ba3e5857b9d804870e2c3b2ea178
fordy13 <jesse@ventureapp.com>
2017-07-27 13:16:34 -0400

    rm this footer too

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Footer from './Footer'
 import Header from './Header'
 import SignIn from '../containers/SignIn'
 
@@ -7,7 +6,6 @@ const App = () => (
   <div>
     <Header />
     <SignIn />
-    <Footer />
   </div>
 )
 
