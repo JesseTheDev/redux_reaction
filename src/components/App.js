2f1c5ae5b608afb325eb070053d235fd3b76ca58
fordy13 <jesse@ventureapp.com>
2017-07-27 11:26:05 -0400

    we have header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from './Footer'
+import Header from './Header'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
+    <Header />
     <AddTodo />
     <VisibleTodoList />
     <Footer />
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const style = {
+  height: '50px',
+  display: 'fixed',
+  top: '0px',
+  left: '0px',
+  right: '0px',
+  'background-color': '#0A131C',
+  color: '#fff'
+};
+
+const Header = () => (
+  <nav style={style}>
+    Header
+  </nav>
+)
+
+export default Header
