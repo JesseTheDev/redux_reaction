cd5e51c2c22096358c6c7950c810df66d2727f8a
Brendan DeStefano <brendandestefano@gmail.com>
2017-07-29 15:57:18 -0400

    its happeningggggg

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,32 @@
 import fetch from 'isomorphic-fetch'
-
+require('isomorphic-form-data')
 export const REQUEST_ACTIVITY = 'REQUEST_ACTIVITY'
 export const RECEIVE_ACTIVITY = 'RECEIVE_ACTIVITY'
 export const CHANGE_FORM = 'CHANGE_FORM'
-
+export const SUBMIT_LOGIN = 'SUBMIT_LOGIN'
 export function changeForm(loginData){
   return {
     type: CHANGE_FORM,
     loginData: loginData
+  }
+}
+
+export function submitLogin(email, password){
+  return (dispatch) => {
+    return dispatch(authenticate(email, password))
+  }
+}
+
+function authenticate(email, password){
+  let form = new FormData();
+  form.append('email',email)
+  form.append('password', password)
+  return dispath => {
+    return fetch(`http://dockerhost:8080/api/authenticate`, {
+      method: 'POST',
+      body: form})
+      .then(response => response.json())
+      .then(json => console.log(json))
   }
 }
 function requestActivity() {
--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import { changeForm } from '../actions'
+import { changeForm, submitLogin } from '../actions'
 
 class LoginForm extends Component {
 
   render() {
     const { email, password } = this.props
     return (
-      <div>
-        <input type="text" value={email} onChange={this.changeEmail.bind(this)}/><input type="text" value={password} onChange={this.changePassword.bind(this)}/><button >Go</button>
-      </div>
+      <form onSubmit={this.submitLogin.bind(this)}>
+        <input type="text" value={email} onChange={this.changeEmail.bind(this)}/><input type="text" value={password} onChange={this.changePassword.bind(this)}/><button type="submit">Go</button>
+      </form>
     )
   }
 
@@ -21,7 +21,12 @@ class LoginForm extends Component {
     const { dispatch } = this.props
     dispatch(changeForm({email: this.props.email, password: e.target.value}))
   }
-  
+  submitLogin(e){
+    const { dispatch } = this.props
+
+    e.preventDefault();
+    dispatch(submitLogin(this.props.email, this.props.password))
+  }
 }
 
 LoginForm.propTypes = {
